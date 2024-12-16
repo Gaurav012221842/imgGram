f3cca59baeb6fb288efd77fb7adf14c3a19463ee
Gaurav Kumar <gauravkumar@Gauravs-MacBook.local>
2024-12-16 22:52:59 +0530

    added aws Config with uploading images

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
+import {createPost} from './controllers/postController.js'; 
+import { s3uploader } from './config/multerConfig.js';
+
 const app=express();
-app.get('/',(req,res) => {
-    res.send('Hello, World!');
-});
-app.get('/ping',(req,res) => {
-    res.json({messagee: 'Hello, World with json response'});}
-);
-app.post('/ping',(req,res) => {
-    res.json({message: 'Hello, World with json response from POST request'});
-});
-app.put('/ping',(req,res) => {
-    res.json({message: 'Hello, World with json response from PUT request'});
-});
-app.delete('/ping',(req,res) => {
-    res.json({message: 'Hello, World with json response from DELETE request'});
-});
+ 
+ 
+app.use(express.json());
+app.post('/posts',s3uploader.single('image'), createPost);
+ 
 app.listen(3000, () =>{ console.log('Server running on port 3000')
         connectDB();
 });
