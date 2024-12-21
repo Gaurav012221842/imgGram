933f92046d3fcaca5916109701d470f45aa818c0
Gaurav Kumar <gauravkumar@Gauravs-MacBook.local>
2024-12-22 02:14:32 +0530

    Added Authorizations Checks

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -36,3 +36,13 @@ export const isAuthenticated = async (req, res, next) => {
     }
 
 }
+
+export const isAdmin = async(req, res, next) => {
+    if(req.user.role!=="admin"){
+        return res.status(403).json({
+            success: false,
+            message: "User is not authorized to perform this action"
+        });
+    }
+    next();
+};
